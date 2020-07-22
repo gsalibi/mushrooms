2dd150608167e3afb3c6b6be8639eef8f4ca2279
sjordine <sjordine@yahoo.com.br>
2020-07-22 17:30:17 -0300

    XCode Tutorial skeleton

---
+++ swift/tutorial/Mushroomer/Mushroomer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mushroomer
+//
+//  Created by SERGIO J RAFAEL ORDINE on 22/07/20.
+//  Copyright © 2020 SERGIO J RAFAEL ORDINE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
